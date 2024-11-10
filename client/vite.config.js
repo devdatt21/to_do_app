6e7d43038f884d1ea1920f8be0cd02314e903e5d
Devdatt Rupapara <devdattrupapara21@gmail.com>
2024-11-10 22:13:51 +0530

    change in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600, // Increased limit to accommodate larger chunks
-    rollupOptions: {
-      output: {
-        manualChunks: id => {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].split('.')[0];
-          }
-        }
-      }
-    },
-    minify: 'terser' // Ensures code is minified using terser, the default minifier
-  }
-})
+  plugins: [react()],
+});
